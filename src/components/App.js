ee6ff3ea6617e3448d42727cc505722573e6b596
David Feldbaum <davidfeldbaum@upshotmail.com>
2018-07-05 13:57:27 -0500

    added project photos for grid, added view switching function to state

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,35 @@ import { BrowserRouter, Link, Router } from 'react-router-dom';
 import '../styles/App.css';
 import Header from './Header/Header'
 import Projects from './Projects/Projects'
+import Contact from './Contact'
 
 class App extends Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      progress: 'corona-summer',
+    }
+  }
+
+  showProjects(){
+    this.setState({progress:"projects"})
+  }
+
+  showCoronaSummer(){
+    this.setState({progress:"corona-summer"})
+  }
+
+  PickView(props){
+    let progress = this.state.progress;
+
+    if (progress == 'projects'){
+      return <Projects/>
+    }
+    if (progress == 'corona-summer'){
+      return <Contact/>
+    }
   }
 
   render() {
@@ -15,8 +39,7 @@ class App extends Component {
       <div className="wrapper">
           <Header/>
 
-          <Projects/>
-
+          {this.PickView()}
 
       </div>
     );
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,46 +4,41 @@ class Projects extends Component {
   render() {
     return (
 
-
-
       <div className="projects-section">
-
         <div class="grid">
-          <div class="cell cell1">
-            <img src="../http://placehold.it/800x800" class="responsive-image"/>
+          <div class="item item1">
+              <a href="/contact"><a href="/contact"><img src="http://placehold.it/800x800"/></a></a>
           </div>
-          <div class="cell">
-            <img src="http://placehold.it/800x800" class="responsive-image"/>
+
+          <div class="item item2">
+            <a href="/contact"><img src="http://placehold.it/800x800"/></a>
           </div>
-          <div class="cell">
-            <img src="http://placehold.it/800x800" class="responsive-image"/>
+
+          <div class="item item3">
+            <a href="/contact"><img src="http://placehold.it/800x800"/></a>
           </div>
-          <div class="cell">
-            <img src="http://placehold.it/800x800" class="responsive-image"/>
+
+          <div class="item item4">
+            <a href="/contact"><img src="http://placehold.it/800x800"/></a>
           </div>
-          <div class="cell">
-            <img src="http://placehold.it/800x800" class="responsive-image"/>
+
+          <div class="item item5">
+            <a href="/contact"><img src="http://placehold.it/800x800"/></a>
           </div>
-          <div class="cell">
-            <img src="http://placehold.it/800x800" class="responsive-image"/>
+
+          <div class="item item6">
+            <a href="/about"><img src="http://placehold.it/800x800"/></a>
           </div>
-          <div class="cell">
-            <img src="http://placehold.it/800x800" class="responsive-image"/>
+
+          <div class="item item7">
+            <a href="/contact"><img src="http://placehold.it/800x800"/></a>
           </div>
-          <div class="cell">
-            <img src="http://placehold.it/800x800" class="responsive-image"/>
+
+          <div class="item item8">
+            <a href="/contact"><img src="http://placehold.it/800x800"/></a>
           </div>
         </div>
-
-
       </div>
-
-
-
-
-
-
-
 
     );
   }
