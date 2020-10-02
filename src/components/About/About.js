d831bb497c61308ce19dff30eee1ece0b4ec375d
David Feldbaum <defeldbaum@gmail.com>
2020-10-02 11:23:27 -0500

    Updated about page bio and image

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -18,11 +18,11 @@ class About extends Component {
 
         <div className="item text-container">
         <h1 className="text-container__title">About</h1>
-        <p className="text-container__copy">My passion is bridging the gap between design and programming. My approach to web design is creative, and holistic. I have a wide range of front-end development skills that include HTML5, CSS3, JavaScript, React.js, Bootstrap, and Flexbox Layouts. I have back-end web development proficiency in Node.js/Express, Sinatra, Ruby on Rails, Gulp, MongoDB and mySQL. I have digital marketing expertise in SEO, SEM (Google AdWords, Bing Ads) and Social Media Advertising.</p>
+        <p className="text-container__copy">My passion is bridging the gap between design and programming. My approach to web design is creative, and holistic. I have a wide range of front-end development skills that include HTML5, CSS3/SASS/LESS, JavaScript, React, Redux, Vue, Angular and Flexbox layouts as well as back-end web development proficiency in Node.js/Express, Ruby on Rails, MongoDB and mySQL. Additionally, I have strong client-level experience working within CMS environments such as AEM, Sitecore and Wordpress. Outside of web development, I have digital marketing expertise in SEO, SEM (Google AdWords, Bing Ads) and Social Media Advertising.</p>
         </div>
 
         <div class="item img-container">
-          <img src="/../img/about-dfeldbaum-3.jpg"/>
+        <img src="/../img/about-dfeldbaum.jpg"/>
         </div>
 
       </div>
